2931caa755e0f893748979c4ccce7f4bee33c019
drissboukhamla <51950895+drissboukhamla@users.noreply.github.com>
2026-02-12 21:15:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -55,31 +55,31 @@ const providers = [
   },
   {
     name: "Florida palace ",
-    category: "Lieu",
+    category: "lieu",
     description: "Situé à Marseille, le Florida Palace vous propose d'organiser une réception de mariage à votre image dans un cadre prestigieux qui fera de votre mariage une célébration inoubliable",
     website: "https://www.mariages.net/salle-mariage/florida-palace--e28573?cmp=ADW-SEM-PMAX-VENUES&utm_source=google&utm_medium=paid-search&utm_campaign=VENUES-Pmax-B2C-Marketplace&utm_content=performance_cpm_search-ad_na_na&gclid=CjwKCAiAkbbMBhB2EiwANbxtbZdJ0OFQiZzCfZol423j1ZuOssEV7sgm-uQbkV3sLi43sRzcjdm0JxoCzdMQAvD_BwE"
   },
   {
     name: "Le Buz",
-    category: "Lieu",
+    category: "lieu",
     description: "Offrez-vous un mariage de prestige dans un décor moderne et design pour votre plus grand plaisir. Le Buz vous accueille à la Valentine à deux pas du Château de la Buzine pour une réception de mariage des plus festives dans un cadre idyllique.",
     website: "https://www.mariages.net/salle-mariage/le-buz--e133244"
   },
    {
     name: "Le Domaine des Calanques",
-    category: "Lieu",
+    category: "lieu",
     description: "Niché aux portes de Cassis, à Marseille, ce lieu d’exception offre un cadre idéal pour organiser vos réceptions, qu’elles soient privées ou professionnelles. ",
     website: "https://www.mariages.net/salle-mariage/le-domaine-des-calanques--e392739"
       },
   {
     name: "Camélia Palace",
-    category: "Lieu",
+    category: "lieu",
     description: "Pour que votre mariage soit une réussite totale et se déroule dans un cadre qui vous garantit joie et convivialité, faites confiance à Camélia Palace. Cette salle accueillante et chaleureuse fera de votre banquet un moment véritablement magique pour vous et tous vos proches. ",
     website: "https://www.mariages.net/salle-mariage/camelia-palace--e139150"
       },
   {
     name: "Espace Arena",
-    category: "Lieu",
+    category: "lieu",
     description: "Futurs mariés, bienvenus à l’Espace Arena ! Ce lieu de réception enchanteur et spacieux est parfait pour une célébration de grande envergure tel que votre mariage ! Les propriétaires avenants et disponibles vous attendent avec impatience pour mettre en place votre plus belle fête. N’attendez plus, venez les rencontrer ! ",
     website: "https://www.mariages.net/salle-mariage/espace-arena--e280676"
       },
